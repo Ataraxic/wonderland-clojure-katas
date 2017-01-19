7cee4587d26ab6a33bbd798b53d6c4861d1d2d38
Justin Sung <justinfsung@gmail.com>
2017-01-19 02:16:52 -0500

    first clojure club meeting

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,38 @@
 (ns wonderland-number.finder)
 
-(defn wonderland-number []
-  ;; calculate me
-  42)
+(def known-numbers '(2 3 4 5 6))
+
+(defn convert-to-seq [number]
+  (map read-string (re-seq #"\d" (str number))))
+
+
+(defn check-number
+  [& args]
+  (if (apply = (map sort (map convert-to-seq args)))
+    true
+    false
+    ))
+
+(first (filter #(not (nil? %)) (list (check-number 12345 54321))))
+
+(defn valid-number [n] 
+  (first 
+   (filter (fn [l] (every? #(true? %) l))
+           (map 
+            (fn [multiplied] (check-number n multiplied)) 
+            (map 
+             (fn [arg] (* n arg)) known-numbers)))))
+
+
+
+(defn valid-number 
+  [n]
+  (if (every? #(true? %) 
+              (map
+               (fn [m] (check-number n m))
+               (map (fn [arg] (* n arg)) known-numbers)))
+   n
+   ))
+(defn wonderland-number [] (filter #(not (nil? %)) (map valid-number (range 100000 1000000))))
+
+(wonderland-number)
