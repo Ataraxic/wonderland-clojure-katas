235e4bd1ba75d8097a7bd83006df3d84adba82f3
Justin Sung <justinfsung@gmail.com>
2017-01-24 02:15:27 -0500

    initial work on maze

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,32 @@
 (ns tiny-maze.solver)
 
+
+(defn map-deep-indexed
+  [f matrix indices]
+  (map-indexed 
+   (fn [idx row-or-val]
+     (if (sequential? row-or-val) 
+       (map-matrix f row-or-val (conj idx indices))
+       (f row-or-val indices))) 
+   matrix))
+
+(map-deep-indexed #(print % %2) [1 2 [1 2 3]] [])
+
+(defn init-point [])
+
+(defn initial-matrix 
+  [matrix]
+  (map-deep init-point matrix))
+
+(defn split-point? [point])
+
+(defn dir-tried? [dir])
+
+(defn move [xpos, ypos,])
+  
+
+
+
 (defn solve-maze [maze])
+
+
